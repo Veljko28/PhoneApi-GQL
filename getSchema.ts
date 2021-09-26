96366bcea4f6f2265a7c50f17be522d520e99133
Veljko28 <charlibear28@gmail.com>
2021-09-26 15:33:55 +0200

    merging all the schemas into one

---
+++ getSchema.ts
@@ -0,0 +1,23 @@
+import { GraphQLSchema } from 'graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { mergeSchemas } from '@graphql-tools/schema';
+import {importSchema} from 'graphql-import';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+export const getSchema: () => GraphQLSchema = () => {
+
+    const schemas: GraphQLSchema[] = [];
+
+    const folders = fs.readdirSync(path.join(__dirname, './modules'));
+
+    folders.forEach(folder => {
+        const {resolvers} = require(path.join(__dirname, `./modules/${folder}/resolvers`));
+        const typeDefs = importSchema(path.join(__dirname, `./modules/${folder}/schema.graphql`));
+
+        schemas.push(makeExecutableSchema({resolvers, typeDefs}));
+    })
+
+    return mergeSchemas({schemas});
+  }
