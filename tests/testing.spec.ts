4067014bc6a949753d231299bf7244f6e2518226
Veljko28 <charlibear28@gmail.com>
2021-09-27 11:48:09 +0200

    testing if the jest config is working

---
+++ tests/testing.spec.ts
@@ -0,0 +1,23 @@
+import { Phone } from "../models/Phone";
+import startServer from "../startServer";
+import 'regenerator-runtime/runtime';
+import mongoose from "mongoose";
+
+// beforeAll( async () => {
+//   await startServer();
+// }) 
+
+// afterAll(() => {
+//   mongoose.connection.close();
+// })
+
+describe("testing if jest works", () => {
+
+  test("first test should pass", async () => {
+    
+    const res = await Phone.find();
+    console.log(res);
+    expect(2+2).toEqual(4);
+    await mongoose.connection.close();
+  })
+})
