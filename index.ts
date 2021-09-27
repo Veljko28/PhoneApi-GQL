b70ce53a5d7873544a916e2741dbdcfea90e5976
Veljko28 <charlibear28@gmail.com>
2021-09-27 11:44:30 +0200

    small changes to start sever and index

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
 import startServer from "./startServer";
 
-
-const index = async () => {
-    await startServer().catch(() => console.log("Failed to start sever"));
-} 
-
-index();
+startServer();
--- startServer.ts
+++ startServer.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { getSchema } from './getSchema';
+import mongoose from 'mongoose';
+import "dotenv/config";
 
 
 const startServer = async () => {
@@ -11,6 +13,8 @@ const startServer = async () => {
           origin: "http://localhost:4000"
   }
   
+ await mongoose.connect(`mongodb://localhost:27017/${process.env.TEST_SERVER === 'true' ? 'phonesTest' : 'phoneDb'}`);
+
   
   const server = new ApolloServer({schema: getSchema()});
   
