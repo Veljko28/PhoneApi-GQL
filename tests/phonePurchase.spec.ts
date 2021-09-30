913d3e3eb0df451149b567f4ee45ab547504b03b
Veljko28 <charlibear28@gmail.com>
2021-09-30 16:01:09 +0200

    adding phone purchase tests

---
+++ tests/phonePurchase.spec.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
+import { PhonePurchase } from '../models/PhonePurchase';
+
+
+describe("Testing Phone Purchases", () => {
+
+    const purchase = {
+        Id: uuidv4(),
+        PhoneId: uuidv4(),
+        SellerId: uuidv4(),
+        BuyerId: uuidv4(),
+        PurchaseDate: Date.now()
+    }
+    
+    test("Adding Phone Purchase", async () => {
+        const phone = new PhonePurchase(purchase);
+        await phone.save(); 
+    })
+
+    test("Find Phone Purchase", async () => {
+        const found = await PhonePurchase.find({Id: purchase.Id});
+        expect(found).toBe(purchase);
+    })
+
+    test("Remove Phone Purcase", async () => {
+        await PhonePurchase.remove({Id: purchase.Id});
+
+        const find = await PhonePurchase.find({Id: purchase.Id});
+
+        expect(find).not.toBe(purchase);
+    })
+})
