1611a1242b3a257ea4fd0f0b395ab6bc7ee57eba
Veljko28 <charlibear28@gmail.com>
2021-09-30 16:00:05 +0200

    fixing a contact model type-o

--- models/ContactSupport.ts
+++ models/ContactSupport.ts
@@ -1,6 +1,6 @@
 import mongoose, {mongo, Schema} from 'mongoose';
 
-export const ConctactSupport = mongoose.model("ConctactSupport", new Schema({
+export const ContactSupport = mongoose.model("ContactSupport", new Schema({
   Id: String,
   Name: String,
   Email: String,
