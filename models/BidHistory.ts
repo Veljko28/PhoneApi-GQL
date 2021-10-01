fe7a3cbf0041a81acf39c91183201307e056e101
Veljko28 <charlibear28@gmail.com>
2021-10-01 20:08:32 +0200

    bid history and subscribe email field now use _id and addding the todo list

--- models/BidHistory.ts
+++ models/BidHistory.ts
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const BidHistory = mongoose.model("BidHistory", new Schema({
-    Id: String,
     Bid_Id: String,
     UserName: String,
     Amount: Number
--- models/SubscribeEmail.ts
+++ models/SubscribeEmail.ts
@@ -1,6 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const SubscribeEmail = mongoose.model("SubscribeEmail", new Schema({
-  Id: String,
   Email: String
 }));
