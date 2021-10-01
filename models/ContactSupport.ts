bc401ce4601937b4fe62ea75caf6c3d7ddfe51b9
Veljko28 <charlibear28@gmail.com>
2021-10-01 20:07:27 +0200

    contact support and notification now use _id

--- models/ContactSupport.ts
+++ models/ContactSupport.ts
@@ -1,7 +1,6 @@
 import mongoose, {mongo, Schema} from 'mongoose';
 
 export const ContactSupport = mongoose.model("ContactSupport", new Schema({
-  Id: String,
   Name: String,
   Email: String,
   Subject: String,
--- models/Notification.ts
+++ models/Notification.ts
@@ -1,7 +1,6 @@
 import mongoose, {mongo, Schema} from 'mongoose';
 
 export const Notification = mongoose.model("Notification", new Schema({
-    Id: String,
     Name: String,
     Type: String,
     UserId: String,
