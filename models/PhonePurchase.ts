bc4e1b88cc6c626cbc5ef4c00b3cfd3fff5bb26b
Veljko28 <charlibear28@gmail.com>
2021-10-01 20:07:58 +0200

    phone purchases and wish list now use _id

--- models/PhonePurchase.ts
+++ models/PhonePurchase.ts
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const PhonePurchase = mongoose.model("PhonePurchase",new Schema({
-  Id: String,
   PhoneId: String,
   SellerId: String,
   BuyerId: String,
--- models/WishList.ts
+++ models/WishList.ts
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 
 export const WishList = mongoose.model("WishList", new Schema({
-  Id: String,
   UserId: String,
   PhoneId: String,
   Type: String
