8fa4be61486ceef32a6fffff9a891af4daa9de1e
Veljko28 <charlibear28@gmail.com>
2021-10-02 13:53:44 +0200

    creating the user resolver and schema with bcrypt and yup testing for register and login

--- modules/user/resolvers.ts
+++ modules/user/resolvers.ts
@@ -1,37 +1,101 @@
 import { User } from '../../models/User';
-
-interface LoginForm {
-    UserName: String,
-    Password: String
-}
+import * as yup from 'yup';
+import bcrypt from 'bcryptjs';
+import { CreateAccessToken } from '../../auth/CreateAccessToken';
 
 interface RegisterForm {
-    UserName: String,
-    Email: String,
-    Password: String,
-    Confirm_Password: String
+  userName: string,
+  email: string,
+  password: string,
+  confirm_password: string
 }
 
+const yupSchema = yup.object().shape( {
+    email: yup.string().min(10).max(255).email(),
+    userName: yup.string().min(5).max(100),
+    password: yup.string().min(6).max(255),
+    confirm_password: yup.string().min(6).max(255),
+}) 
 
-const resolver = {
-    Query: {
-        getUser: async (_: any, {_id}: {_id: string}) => {
-            const user = await User.find({_id});
-            return user;
-        }
+export const resolvers = {
+  Query: {
+    getUser:  async (_: any, args: {id: string}) => {
+      const {id} = args;
+      const user = await User.find({_id: id});
+
+      return user;
     },
-    Mutation: {
-        login: async (_: any, {user} : {user: LoginForm}) => {
-            // check for username / get user by username
+    getAll: async () => {
+      const allUsers = await User.find();
 
-            // bcrypt the given password and compare with one in db
-        },
-        register: async (_: any, {user}: {user: RegisterForm}) => {
-            // check if username in use 
-
-            // check if email in use
-
-            // create user
-        } 
+      return allUsers;
     }
+  },
+  Mutation: {
+    register: async (_: any, args: {registerForm: RegisterForm}) => {
+      
+      const {registerForm} = args;
+      
+      try {
+             await yupSchema.validate(registerForm, { abortEarly: false });
+      } 
+      catch (err) {return false}
+      
+      if ( registerForm.confirm_password !== registerForm.password) return false;
+
+      const hashedPassword = bcrypt.hashSync(registerForm.password, 10);
+
+      const userSchema = {
+        Email: registerForm.email,
+        UserName: registerForm.userName,
+        Password: hashedPassword,
+        Description: "",
+        Rating: 0,
+        PhoneNumber: "",
+        Phones_sold: 0,
+        EmailConfirmed: false,
+        LoyalityPoints: 0
+      };
+
+
+      // const found = await User.find({email: registerForm.email});
+      // console.log(found);
+
+      // if (found !== null) { // if the email is already in use
+      //   return false;
+      // }
+   
+      const user = new User(userSchema);
+      await user.save();
+
+      return true;
+    },
+    
+    login: async (_: any,args: {info: {email: string,password: string}}) => {
+
+      try {
+            await yupSchema.validate(args.info, {abortEarly: false});
+          }
+          catch (err){
+              return null;
+          }
+
+          const {email, password} = args.info;
+
+          const user = await User.findOne({email});
+
+         if (user === null) return null;
+
+          bcrypt.compare(password, (user as any).password , (err) => {
+              if (err){
+                    return null;
+                }
+            })
+
+      return {
+        accessToken: CreateAccessToken(user.id, '15m')
+      };            
+    }
+
+  }
 }
