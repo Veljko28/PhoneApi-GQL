dd9ed5675584d8bef43238f80ea5246293195c1e
Veljko28 <charlibear28@gmail.com>
2021-10-02 13:50:57 +0200

    checking if the user is authenticated

---
+++ auth/isAuth.ts
@@ -0,0 +1,22 @@
+import { verify } from "jsonwebtoken";
+import { skip } from 'graphql-resolvers';
+import { MyContext } from "../types/MyContext";
+
+
+export const isAuth = (_p: any, _a: any, ctx: MyContext) => {
+    const authorization = ctx.req.headers['authorization'];
+
+    if (!authorization) {
+        throw new Error("not authenticated");
+    }
+
+    try {
+        const token = authorization.split(" ")[1];
+        const addPayload = verify(token, process.env.SECRET!);
+        (ctx.payload as any) = addPayload;
+    } catch(err){
+        throw new Error("not authenticated");
+    }
+    
+    return skip;
+}
