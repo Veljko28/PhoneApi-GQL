82fad9231c69bef937e2547ab6a517996de39c7d
Veljko28 <charlibear28@gmail.com>
2021-10-05 10:52:53 +0200

    adding user ts interface

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,20 @@
 import mongoose, { Schema } from 'mongoose';
 
-export const User = mongoose.model("User",new Schema({
+export interface IUser extends mongoose.Document {
+    Id: String,
+    Email: String,
+    UserName: String,
+    Password: String,
+    Description: String,
+    Rating: Number,
+    PhoneNumber: String,
+    Phones_sold: Number,
+    EmailConfirmed: Boolean,
+    LoyalityPoints: Number
+}
+
+
+export const User = mongoose.model<IUser>("User",new Schema({
     Id: String,
     Email: String,
     UserName: String,
