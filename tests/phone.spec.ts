67b68f74ec5b0c21afe7ee51ec1a89cbb83de852
Veljko28 <charlibear28@gmail.com>
2021-10-05 10:55:10 +0200

    fixing almost all the phone tests except find

--- tests/phone.spec.ts
+++ tests/phone.spec.ts
@@ -1,31 +1,75 @@
+import startServer from "../startServer";
 import { graphqlTestCall } from "./assets/graphqlTestCall";
+import mongoose from 'mongoose';
 
 const addMutation = `
-mutation {
-  addPhone(model: {
-    Name: "Testing phone",
-    Description: "test desc",
-    Image: "",
-    Seller: "neko_tamo",
-    Price: 150,
-    Category: "Test",
-    Brand: "test",
-  })
+mutation($addPhoneModel: AddPhoneModel) {
+  addPhone(model: $addPhoneModel)
 }`;
 
-const deleteMutation = `
-`
+let phoneId: string;
+
+beforeAll( async () => {
+  await startServer();
+})
+
+afterAll( async () => {
+  await mongoose.connection.close();
+})
 
 describe("Testing Phones", () => {
     test("Adding a phone", async () => {
-        const response = await graphqlTestCall(addMutation);
+
+       const phoneModel = {
+        Name: "Testing phone",
+        Description: "test desc",
+        Image: "",
+        Seller: "neko_tamo",
+        Price: 150,
+        Category: "Test",
+        Brand: "test",
+       }
+
+        const response = await graphqlTestCall(addMutation, {addPhoneModel: phoneModel});
 
         expect(response).toEqual({data: {addPhone: true}});
   })
 
+  test("Finding a phone", async () => {
+    const allPhones = await graphqlTestCall(`
+      {
+        getAllPhones {
+          Id
+        }
+      }
+    `);
+    console.log(allPhones.data.getAllPhones);
+    const {phones} = allPhones.data.getAllPhones;
+
+    expect(phones.length).toBeGreaterThan(0);
+
+    phoneId = phones[0].Id;
+
+    const phone = await graphqlTestCall(`
+    {
+      getPhone(id: "${phoneId}"){
+        Name
+      }
+    }`);
+
+    const {phoneObj} = phone.data.getPhone;
+
+    expect(phoneObj.Name).toBeTruthy();
+  })
+
 
   test("Deleting the added phone", async () => {
+      const del = await graphqlTestCall(`
+      mutation {
+        deletePhone(id: "${phoneId}")
+      }`);
 
+      expect(del).toEqual({data: { deletePhone: true }});
   })
 
 })
