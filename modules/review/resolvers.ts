3a83fa9fde13376fab5dcde1308ebed4bed7fc2e
Veljko28 <charlibear28@gmail.com>
2021-10-08 12:00:07 +0200

    adding the review resolvers and gql schema

---
+++ modules/review/resolvers.ts
@@ -0,0 +1,35 @@
+import { v4 } from "uuid";
+import { IUserReview, UserReview } from "../../models/UserReview"
+
+export const resolvers = {
+  Query: {
+    getUserReviews: async (_ :any, args: {UserId: String}) => {
+      const { UserId } = args;
+      const reviews = await UserReview.find({SellerId: UserId}); 
+      return reviews;
+    },
+    checkReviewed: async (_: any, args: {BuyerId: String, PhoneId: String}) => {
+      const { BuyerId, PhoneId } = args;
+      const reviewed = await UserReview.find({BuyerId, PhoneId});
+      return reviewed.length !== 0;
+    }
+  },
+  Mutation: {
+    addReview: async (_: any, args: {model: IUserReview}) => {
+      const { model } = args;
+      const review = new UserReview({...model,
+      Id: "Id" in model ? model.Id : v4(),
+      DateCreated: Date.now() });
+
+      try {
+        await review.save();
+        return true;
+      }
+      catch(err) {
+        console.log(err);
+        return false;
+      }
+    },
+
+  }
+}
