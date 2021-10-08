11cdd31f3ae9d9632641d98f535ff8773d9252a2
Veljko28 <charlibear28@gmail.com>
2021-10-08 12:01:39 +0200

    wish list and user review ts types

--- models/UserReview.ts
+++ models/UserReview.ts
@@ -1,6 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
 
+export interface IUserReview extends mongoose.Document {
+  Id: String,
+  Rating: Number,
+  BuyerId: String,
+  SellerId: String,
+  PhoneId: String,
+  DateCreated: Number,
+  Message: String
+}
+
 export const UserReview = mongoose.model("UserReview", new Schema({
+  Id: String,
   Rating: Number,
   BuyerId: String,
   SellerId: String,
--- models/WishList.ts
+++ models/WishList.ts
@@ -1,6 +1,14 @@
 import mongoose, { Schema } from 'mongoose';
 
+export interface IWishList extends mongoose.Document {
+  Id: String,
+  UserId: String,
+  PhoneId: String,
+  Type: String
+}
+
 export const WishList = mongoose.model("WishList", new Schema({
+  Id: String,
   UserId: String,
   PhoneId: String,
   Type: String
